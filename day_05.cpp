146e31d9e7a61c284c690c7ca8f999b32f5b8ef8
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-05 10:37:43 +0300

    day 5 commit

---
+++ day_05.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <bitset>
+
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+class Solution {
+public:
+    int firstUniqChar(const string &s) {
+        unordered_map<char, int> map(26);
+        for (auto ch : s) ++map[ch];
+        for (auto i = 0; i < s.size(); ++i) if (map[s[i]] == 1) return i;
+        return -1;
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    string str = "loveleetcode";
+    std::cout << solution.firstUniqChar(str) << std::endl;
+    return 0;
+}
