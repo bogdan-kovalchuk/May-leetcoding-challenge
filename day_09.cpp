1e20b72415a3b630aa6361e583cdc9e153de8574
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-09 17:24:59 +0300

    day 9 commit

---
+++ day_09.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+    }
+};
+
+int main() {
+    int num = 16;
+    Solution solution = Solution();
+    std::cout << solution.isPerfectSquare(num) << std::endl;
+    return 0;
+}
