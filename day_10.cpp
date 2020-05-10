377bc8ed06b4f283901f439331e6fc3e9f8e0d34
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-10 12:54:28 +0300

    day 10 commit

--- day_10.cpp
+++ day_10.cpp
@@ -10,17 +10,28 @@ using std::unordered_map;
 class Solution {
 public:
     int findJudge(int N, vector<vector<int>> &trust) {
+        vector<int> for_whom_voted_count(N + 1, 0);
+        vector<int> who_voted_count(N + 1, 0);
 
+        for (const auto &t : trust) {
+            for_whom_voted_count[t[1]]++;
+            who_voted_count[t[0]]++;
+        }
+        for (int i = 1; i <= N; ++i) {
+            if ((for_whom_voted_count[i] == N - 1) &&
+                (who_voted_count[i] == 0)) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
 
 int main() {
-    int N = 4;
+    int N = 3;
     vector<vector<int>> trust = {{1, 3},
-                                 {1, 4},
                                  {2, 3},
-                                 {2, 4},
-                                 {4, 3}};
+                                 {3, 1}};
 
     Solution solution = Solution();
     std::cout << solution.findJudge(N, trust) << std::endl;
