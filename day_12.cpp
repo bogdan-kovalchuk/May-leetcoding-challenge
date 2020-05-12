ce7b8e62931709e7d2f41430a8e0f7f29b053f0d
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-12 14:30:09 +0300

    day 12 commit

---
+++ day_12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int> &nums) {
+
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+
+    Solution solution = Solution();
+    std::cout << solution.singleNonDuplicate(nums) << std::endl;
+
+    return 0;
+}
