62a9c69db6d801dde69165f32a42dc2bd15a1dba
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-12 20:56:12 +0300

    day 12 commit

--- day_12.cpp
+++ day_12.cpp
@@ -10,12 +10,26 @@ using std::unordered_map;
 class Solution {
 public:
     int singleNonDuplicate(vector<int> &nums) {
+        if (nums.size() == 1) return nums[0];
+        int l = 0, r = nums.size() - 1, m = 0;
 
+        while (r - l > 2) {
+            m = l + (r - l) / 2;
+            if (nums[m] == nums[m - 1]) {
+                if ((m - l + 1) % 2 == 0) l = m + 1;
+                else r = m;
+            } else if (nums[m] == nums[m + 1]) {
+                if ((r - m + 1) % 2 == 0) r = m - 1;
+                else l = m;
+            } else return nums[m];
+        }
+        if (nums[l] == nums[l + 1]) return nums[r];
+        else return nums[l];
     }
 };
 
 int main() {
-    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
 
     Solution solution = Solution();
     std::cout << solution.singleNonDuplicate(nums) << std::endl;
