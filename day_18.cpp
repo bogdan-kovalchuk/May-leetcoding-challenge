a9815cb96c4485b5b8c07da46a4ab447793e12f8
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-18 21:21:12 +0300

    day 18 commit

--- day_18.cpp
+++ day_18.cpp
@@ -7,21 +7,21 @@ using std::vector;
 class Solution {
 public:
     bool checkInclusion(const string &s1, const string &s2) {
-        if(s1.size() > s2.size() || s2.empty()) return false;
+        if (s1.size() > s2.size() || s2.empty()) return false;
 
         vector<int> s1v(26, 0), s2v(26, 0);
 
-        for(int i = 0; i < s1.size(); ++i) {
+        for (int i = 0; i < s1.size(); ++i) {
             ++s1v[s1[i] - 'a'];
             ++s2v[s2[i] - 'a'];
         }
 
         if (s1v == s2v) return true;
 
-        for(int i = s1.size(); i < s2.size(); ++i){
+        for (int i = s1.size(); i < s2.size(); ++i) {
             ++s2v[s2[i] - 'a'];
             --s2v[s2[i - s1.size()] - 'a'];
-            if(s1v == s2v) return true;
+            if (s1v == s2v) return true;
         }
         return false;
     }
@@ -32,7 +32,7 @@ int main() {
     string s2 = "a";
 
     Solution solution = Solution();
-    std::cout <<  solution.checkInclusion(s1, s2) << std::endl;
+    std::cout << solution.checkInclusion(s1, s2) << std::endl;
 
     return 0;
 }
