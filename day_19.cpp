95b3e9eb147a57ff07c4ce6c73cef528602b7508
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-19 19:26:37 +0300

    day 19 commit

--- day_19.cpp
+++ day_19.cpp
@@ -28,23 +28,12 @@ private:
 
 int main() {
     auto *S = new StockSpanner();
-//    std::cout << S->next(100) << std::endl;
-//    std::cout << S->next(80) << std::endl;
-//    std::cout << S->next(60) << std::endl;
-//    std::cout << S->next(70) << std::endl;
-//    std::cout << S->next(60) << std::endl;
-//    std::cout << S->next(75) << std::endl;
-//    std::cout << S->next(85) << std::endl;
-
-//    std::cout << S->next(31) << std::endl; // returns 1,
-//    std::cout << S->next(41) << std::endl; // returns 1,
-//    std::cout << S->next(48) << std::endl; // returns 1,
-//    std::cout << S->next(40) << std::endl; // returns 2,
-//    std::cout << S->next(79) << std::endl; // returns 1,
-
-    std::cout << S->next(28) << std::endl; // returns 1,
-    std::cout << S->next(28) << std::endl; // returns 1,
-    std::cout << S->next(28) << std::endl; // returns 1,
-    std::cout << S->next(28) << std::endl; // returns 1,
+    std::cout << S->next(100) << std::endl;
+    std::cout << S->next(80) << std::endl;
+    std::cout << S->next(60) << std::endl;
+    std::cout << S->next(70) << std::endl;
+    std::cout << S->next(60) << std::endl;
+    std::cout << S->next(75) << std::endl;
+    std::cout << S->next(85) << std::endl;
     return 0;
 }
