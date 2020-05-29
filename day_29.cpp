147472843bcfb6d38a475b3e8741c11aa61bb698
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-29 16:37:54 +0300

    day 29 commit

---
+++ day_29.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using std::vector;
+using std::unordered_set;
+using std::unordered_map;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<int> visited(numCourses);
+
+        for (const auto &p : prerequisites) graph[p[0]].emplace_back(p[1]);
+
+        for (int i = 0; i < numCourses; ++i)
+            if (visited[i] == 0 && !dfs(graph, visited, i)) return false;
+
+        return true;
+    }
+
+    bool dfs(const vector<vector<int>> &graph, vector<int> &visited, int v) {
+        if (visited[v] == 1) return false;
+
+        visited[v] = 1;
+
+        for (auto u : graph[v]) {
+            if (!dfs(graph, visited, u)) return false;
+        }
+
+        visited[v] = 2;
+
+        return true;
+    }
+};
+
+int main() {
+    int numCourses = 4;
+    vector<vector<int>> prerequisites = {{1, 0},
+                                         {2, 1},
+                                         {2, 3},
+                                         {3, 2}};
+
+    Solution solution = Solution();
+    std::cout << solution.canFinish(numCourses, prerequisites) << std::endl;
+
+    return 0;
+}
