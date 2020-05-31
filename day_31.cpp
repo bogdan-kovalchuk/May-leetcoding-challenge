8093c57fc1861db1977d17130347284341c24425
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-05-31 22:04:04 +0300

    day 31 commit

---
+++ day_31.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+
+using std::vector;
+using std::map;
+using std::string;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        return 0;
+    }
+};
+
+int main() {
+    string word1 = "intention", word2 = "execution";
+
+    Solution solution = Solution();
+    std::cout << solution.minDistance(word1, word2) << std::endl;
+
+    return 0;
+}
